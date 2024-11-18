50f030042519e4e5c78b654e76e003d62b9e253a
Favio <favio.alikhan30@gmail.com>
2024-11-18 11:24:58 -0500

    railway-runtime

--- backend/settings.py
+++ backend/settings.py
@@ -94,13 +94,14 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 # SQLITE
-"""
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 """
 # POSTGRESQL
 DATABASES = {
@@ -113,7 +114,7 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
